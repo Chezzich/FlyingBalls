88655c82adf8bf4841effda63dd91cfa00e0d715
Bohdan <2002bogdan1407@gmail.com>
2021-05-14 21:11:42 +0300

    initial

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    static public bool goalMet = false;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Projectile")
+        {
+            int castleNum = 0;
+            if (PlayerPrefs.HasKey("Castle"))
+                castleNum = Mathf.Clamp(PlayerPrefs.GetInt("Castle"), 0, 1);
+            castleNum++;
+            PlayerPrefs.SetInt("Castle", castleNum);
+            SceneManager.LoadScene("Scene_0");
+            Goal.goalMet = true;
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color; c.a = 1; mat.color = c;
+        }
+    }
+}
---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    static private Slingshot S;
+    [Header("Set in Inspector")]
+    public GameObject prefabProjectile;
+    public float velocityMult;
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    private Rigidbody projectileRigidBody;
+    [SerializeField] private List<GameObject> castles;
+    
+    static public Vector3 LAUNCH_POS
+    {
+        get 
+        {
+            if (!S)
+                return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
+    private void Awake()
+    {
+        S = this;
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position; 
+        int castleNum = 0;
+        if (PlayerPrefs.HasKey("Castle"))
+            castleNum = Mathf.Clamp(PlayerPrefs.GetInt("Castle"), 0, 2);
+        Instantiate(castles[castleNum], new Vector3(40f, -9.5f), Quaternion.identity);
+    }
+
+    void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        //projectile.GetComponent<Rigidbody>().isKinematic = true;
+        projectileRigidBody = projectile.GetComponent<Rigidbody>();
+        projectileRigidBody.isKinematic = true;
+    }
+
+    private void Update()
+    {
+        if (!aimingMode) return;
+
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        Vector3 mouseDelta = mousePos3D - launchPos;
+
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        {
+            aimingMode = false;
+            projectileRigidBody.isKinematic = false;
+            projectileRigidBody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
+            projectile = null;
+        }
+    }
+}
